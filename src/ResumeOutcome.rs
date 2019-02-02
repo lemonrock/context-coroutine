02d38419fe469708093a1f38d459ecdc20805b7e
Raphael Cohn <raphael.cohn@stormmq.com>
2019-02-02 09:09:02 +0000

    Copyright attribution correction

--- src/ResumeOutcome.rs
+++ src/ResumeOutcome.rs
@@ -1,5 +1,5 @@
-// This file is part of linux-epoll. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/linux-epoll/master/COPYRIGHT. No part of linux-epoll, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
-// Copyright © 2019 The developers of linux-epoll. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/linux-epoll/master/COPYRIGHT.
+// This file is part of context-coroutines. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/context-coroutines/master/COPYRIGHT. No part of context-coroutines, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+// Copyright © 2019 The developers of context-coroutines. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/context-coroutines/master/COPYRIGHT.
 
 
 /// Outcome of a coroutine's resumption.
